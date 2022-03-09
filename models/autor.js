e390d8705ac0747e1ca4ba692d5c6cbc3c741359
Wellen Pereira <86011091+WellenPereiira@users.noreply.github.com>
2022-03-09 20:44:30 +0000

    criação da rota autores e a função de inserção

--- models/autor.js
+++ models/autor.js
@@ -1,18 +1,23 @@
 const db = require("../db")
 
 class Autor {
-  static inserir(){
+  static async inserir(data){
     const connect = await db.conncet();
-    return await connect.query("");
+    const sql = "insert into autores( nome, sobrenome, data_de_nascimento) values($1, $2, $3)";
+    const values = [data.nome, data.sobrenome, data.data_de_nascimento]
+    return await connect.query(sql, values);
   }
+  
   static async selecionar(){
     const connect = await db.conncet();
     return await connect.query("select * from autores");
   }
+  
   static atualizar(){
    const connect = await db.conncet();
     return await connect.query(""); 
   }
+  
   static deletar(){
    const connect = await db.conncet();
     return await connect.query("");
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const Autor = require("../models/autor"); 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* rota para autores */
+router.get("/", async function(req, resp, next){
+  const autores = await Autor.selecionar();
+  res.json(autores.rows);
+})
 
 module.exports = router;
