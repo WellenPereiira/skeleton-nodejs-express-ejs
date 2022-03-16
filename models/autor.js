3a87b203679d6c238545169b7854fcaaf8850981
Wellen Pereira <86011091+WellenPereiira@users.noreply.github.com>
2022-03-16 21:12:39 +0000

    criação das novas rotas atualizar, inserir e deletar

--- models/autor.js
+++ models/autor.js
@@ -4,7 +4,7 @@ class Autor {
   static async inserir(data){
     const connect = await db.connect();
     const sql = "INSERT INTO autores( nome, sobrenome, data_de_nascimento) VALUES ($1, $2, $3)";
-    const values = [data.nome, data.sobrenome, data.data_de_nascimento]
+    const values = [data.nome, data.sobrenome, data.data_de_nascimento];
     return await connect.query(sql, values);
   }
   
@@ -13,14 +13,14 @@ class Autor {
     return await connect.query("select * FROM autores");
   }
   
-  static async atualizar(data, id){
+  static async atualizar(data){
    const connect = await db.connect();
    const sql = "UPDATE autores SET nome=$1, sobrenome=$2, data_de_nascimento=$3 WHERE id=$4"
-   const values = [data.nome, data.sobrenome, data.data_de_nascimento, id]
+   const values = [data.nome, data.sobrenome, data.data_de_nascimento, id];
    return await connect.query(sql, values); 
   }
   
-  static async deletar(data, id){
+  static async deletar(data){
    const connect = await db.connect();
     const sql = "DELETE FROM autores WHERE id=$1"
     return await connect.query(sql, id);
--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,21 @@ router.get('/', async function(req, res, next){
   res.json(autores.rows);
 });
 
-router.get('/', async function(req, res, next){
-  const autores = await Autor.atualizar(data, id);
+router.get('/atualizar', async function(req, res, next){
+  const autor = {nome:"Vilma", sobrenome:"Domingos", data_de_nascimento:"1967/02/19", id:"1"}
+  const autores = await Autor.atualizar(autor);
   res.json(autores.rows);
 });
 
-router.get('/', async function(req, res, next){
-  const autores = await Autor.deletar(data, id);
+router.get('/deletar', async function(req, res, next){
+  const autor = {id:"1"}
+  const autores = await Autor.deletar(autor);
   res.json(autores.rows);
 });
 
-router.get('/', async function(req, res, next){
-  const autores = await Autor.inserir();
+router.get('/inserir', async function(req, res, next){
+  const autor = {nome:"Cirilo", sobrenome:"Pereira", data_de_nascimento:"1967/06/03"}
+  const autores = await Autor.inserir(autor);
   res.json(autores.rows);
 });
 module.exports = router;
